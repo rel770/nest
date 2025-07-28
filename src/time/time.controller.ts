38d759e328751e5f2eb01b9fccb56dfa8d16d0ed
rel700 <rel@mail.com>
2025-07-28 11:48:41 +0300

    Add @Get() getTime() to TimeController

--- src/time/time.controller.ts
+++ src/time/time.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('time')
-export class TimeController {}
+export class TimeController {
+  @Get()
+  getTime(): string {
+    // Return current time in readable format
+    const currentTime = new Date().toLocaleString();
+    return `Current time is: ${currentTime}`;
+  }
+}
