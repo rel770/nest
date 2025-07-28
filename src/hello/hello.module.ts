ef38ce2fb5aa96853bb07a6ad64e3a47279921ce
rel700 <rel@mail.com>
2025-07-28 11:29:54 +0300

    Add HelloController (nest generate controller hello)

--- src/hello/hello.module.ts
+++ src/hello/hello.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { HelloController } from './hello.controller';
 
-@Module({})
+@Module({
+  controllers: [HelloController]
+})
 export class HelloModule {}
